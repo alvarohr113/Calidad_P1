112b1e9080b7aba5cd351b85fda16beb7c09f67e
alvarohr113 <alvaro201102@gmail.com>
2024-09-27 11:14:46 +0200

    anadido readme

--- src/aplicacion.java
+++ src/aplicacion.java
@@ -4,6 +4,9 @@ public class aplicacion {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		
+		
+		System.out.println("Probar dos");
 
 	}
 
